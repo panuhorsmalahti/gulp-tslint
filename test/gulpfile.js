63bdc6b7f984f1c75c9749e093491ad206fbec62
Panu Horsmalahti <panu.horsmalahti@iki.fi>
2015-03-10 21:05:01 +0200

    Add test for template strings. Update few dependencies. Minor style fixes. Version bump.

--- test/gulpfile.js
+++ test/gulpfile.js
@@ -15,19 +15,25 @@ var testReporter = function (output) {
 
 // Prints nothing
 gulp.task('valid', function(){
-      gulp.src('valid.ts')
+    gulp.src('valid.ts')
         .pipe(tslint())
         .pipe(tslint.report('json'));
 });
 
 gulp.task('missing', function(){
-      gulp.src('missing_file.ts')
+    gulp.src('missing_file.ts')
         .pipe(tslint())
         .pipe(tslint.report('json'));
 });
 
+gulp.task('template', function(){
+    gulp.src('template-strings.ts')
+        .pipe(tslint())
+        .pipe(tslint.report('prose'));
+});
+
 gulp.task('invalid-emit', function(){
-      gulp.src(['invalid.ts', 'invalid2.ts'])
+    gulp.src(['invalid.ts', 'invalid2.ts'])
         .pipe(tslint())
         .pipe(tslint.report('prose', {
           emitError: true
@@ -35,20 +41,20 @@ gulp.task('invalid-emit', function(){
 });
 
 gulp.task('invalid-relative', function(){
-      gulp.src(['relative/invalid.ts'], {
+    gulp.src(['relative/invalid.ts'], {
         base: __dirname
-      }).pipe(tslint())
-        .pipe(tslint.report('verbose', {
+    }).pipe(tslint())
+      .pipe(tslint.report('verbose', {
           emitError: true
-        }));
+      }));
 });
 
 // TODO: Find why this doesn't throw an error to the command line
 gulp.task('invalid-emit-return', function(){
-      return gulp.src(['invalid.ts', 'invalid2.ts'])
+    return gulp.src(['invalid.ts', 'invalid2.ts'])
         .pipe(tslint())
         .pipe(tslint.report('prose', {
-          emitError: true
+            emitError: true
         }));
 });
 
@@ -59,15 +65,15 @@ gulp.task('invalid-emptyoptions', function(){
 });
 
 gulp.task('invalid-noemit', function(){
-      gulp.src('invalid.ts')
+    gulp.src('invalid.ts')
         .pipe(tslint())
         .pipe(tslint.report('prose', {
-          emitError: false
+            emitError: false
         }));
 });
 
 gulp.task('invalid-all', function(){
-      gulp.src('invalid.ts')
+    gulp.src('invalid.ts')
         .pipe(tslint())
         .pipe(tslint.report('json', { emitError: false}))
         .pipe(tslint.report('prose', { emitError: false}))
@@ -77,57 +83,57 @@ gulp.task('invalid-all', function(){
 });
 
 gulp.task('invalid-json', function(){
-      gulp.src('invalid.ts')
+    gulp.src('invalid.ts')
         .pipe(tslint())
         .pipe(tslint.report('json'));
 });
 
 gulp.task('invalid-prose', function(){
-      gulp.src('invalid.ts')
+    gulp.src('invalid.ts')
         .pipe(tslint())
         .pipe(tslint.report('prose'));
 });
 
 gulp.task('invalid-verbose', function(){
-      gulp.src('invalid.ts')
+    gulp.src('invalid.ts')
         .pipe(tslint())
         .pipe(tslint.report('verbose'));
 });
 
 gulp.task('invalid-full', function(){
-      gulp.src('invalid.ts')
+    gulp.src('invalid.ts')
         .pipe(tslint())
         .pipe(tslint.report('full'));
 });
 
 gulp.task('invalid-custom', function(){
-      gulp.src('invalid.ts')
+    gulp.src('invalid.ts')
         .pipe(tslint())
         .pipe(tslint.report(testReporter));
 });
 
 gulp.task('no-custom-rules-defined', function(){
-      gulp.src('customRule.ts')
+    gulp.src('customRule.ts')
         .pipe(tslint())
         .pipe(tslint.report('verbose'));
 });
 
 gulp.task('custom-rules-defined', function(){
-      gulp.src('customRule.ts')
+    gulp.src('customRule.ts')
         .pipe(tslint({
-          rulesDirectory: 'rules/'
+            rulesDirectory: 'rules/'
         }))
         .pipe(tslint.report('verbose'));
 });
 
 // Shouldn't report errors because invalid.ts doesn't break the class-name rule
 gulp.task('invalid-json-rules', function(){
-      gulp.src('invalid.ts')
+    gulp.src('invalid.ts')
         .pipe(tslint({
             configuration: {
-              rules: {
-                "class-name": true
-              }
+                rules: {
+                  "class-name": true
+                }
             }
         }))
         .pipe(tslint.report('verbose'));
