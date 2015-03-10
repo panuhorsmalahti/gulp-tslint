0041c72e6475df0cda9193a78f5487113f1e726f
Panu Horsmalahti <panu.horsmalahti@iki.fi>
2015-03-10 21:07:30 +0200

    Minor style fix.

--- test/template-strings.ts
+++ test/template-strings.ts
@@ -1,6 +1,6 @@
 // Both template strings should work.
 if (true) {
-	throw new Error(`ruleSet "${ruleSet.description}" has no defined output for type: ${rule.type}`);
+    throw new Error(`ruleSet "${ruleSet.description}" has no defined output for type: ${rule.type}`);
 } else {
     throw new Error(`ruleSet "${ruleSet.description}" has no defined output tied to type: ${rule.type}`);
 }
