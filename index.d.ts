c0507b4ff2b81135839707c149f765a11d892b3d
Damon Smith <damon.smith@sportsbet.com.au>
2017-06-06 12:17:21 +1000

    changed back to full type defs because gulp-tslint doesn't ship it's ts file

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,42 @@
 /// <reference types="node" />
 
-import { PluginOptions, ReportOptions, TslintFile } from "./index"
-import * as tslintPlugin from "./index"
+declare namespace tslintPlugin {
+
+	interface PluginOptions {
+		configuration?: any;
+		fix?: boolean;
+		formatter?: string | Function;
+		formattersDirectory?: string;
+		rulesDirectory?: string;
+		tslint?: any;
+		program?: any;
+	}
+
+	interface ReportOptions {
+		emitError?: boolean;
+		reportLimit?: number;
+		summarizeFailureOutput?: boolean;
+		allowWarnings?: boolean;
+	}
+
+	interface TslintFile {
+		tslint: any;
+		path: string;
+		relative: string;
+		contents: Buffer | any;
+		isStream(): boolean;
+		isNull(): boolean;
+	}
+
+	interface TslintPlugin {
+		(pluginOptions?: PluginOptions): any;
+		report: (options?: ReportOptions) => any;
+		pluginOptions: PluginOptions;
+	}
+
+	function report(options?: ReportOptions): any;
+    const pluginOptions: PluginOptions;
+}
 
 /**
  * Main plugin function
@@ -9,5 +44,6 @@ import * as tslintPlugin from "./index"
  * Optional.
  * @returns {any}
  */
+declare function tslintPlugin(pluginOptions?: tslintPlugin.PluginOptions): any;
 
 export = tslintPlugin;
